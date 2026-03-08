ae24238bb6557e2ec7f148e4ba60d25be192c297
andrejcredo-byte <andrejcredo@gmail.com>
2026-03-08 03:04:58 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,10 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 );
+function setAppHeight() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--app-height', `${vh}px`);
+}
+
+setAppHeight();
+window.addEventListener('resize', setAppHeight);
